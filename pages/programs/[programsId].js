874474a2976ee63c4a15e8600df4a8b4b279703c
Mehmet <mathdenizi@gmail.com>
2022-12-13 16:34:54 +0100

    cursed project

---
+++ pages/programs/[programsId].js
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+
+function ProgramDetails() {
+  const router = useRouter();
+  console.log(router.query.newsId);
+  const newsId = router.query.newsId;
+  // send a request to the backend API
+  // to fetch the news item with newsId
+
+  return;
+  <>
+    <h1>The Detail Page</h1>;<p1>{newsId}</p1>
+  </>;
+}
+
+export default ProgramDetails;
---
+++ pages/programs/index.js
@@ -0,0 +1,22 @@
+// our-domain/news
+import Link from "next/link";
+
+import { Fragment } from "react";
+
+function ProgramsPage() {
+  return (
+    <Fragment>
+      <h1>The Programs Page</h1>
+      <ul>
+        <li>
+          <Link href="/programs/java-core">Java Core</Link>
+        </li>
+        <li>
+          <Link href="/programs/react">Math will learn react</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default ProgramsPage;
