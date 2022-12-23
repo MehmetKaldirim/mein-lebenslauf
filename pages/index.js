7a8abd1d7e981de80f051cbe8676c97e5e98226a
MehmetKaldirim <mathdenizi@gmail.com>
2022-12-23 12:00:32 +0100

    i retrieve the data my own ser rest api with axios

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import ProgramList from "../components/programs/ProgramList";
+import React, { useState,useEffect } from 'react';
+import axios from "axios";
 
 const DUMMY_PROGRAMS = [
   {
@@ -27,14 +29,50 @@ const DUMMY_PROGRAMS = [
 ];
 
 function HomePage(props) {
-  return <ProgramList programs={props.programs} />;
+  const [movies,setMovies] = useState([]);
+
+ /* function fetchPrograms(){
+    fetch('http://172.20.10.3:8081/programs/api/v3').then(
+      (response)=>{ return response.json();}).then((data)=>{
+        const transformedPrograms = data.data.map(program=> {
+        return {
+          
+          id : program.id,
+          title : program.programName,
+          duration : program.duration,
+          completedTime : program.studyProgres,
+          description : "Bu da description"
+
+        }
+        });
+       setMovies(transformedPrograms) 
+       });
+  }
+  console.log("here i fetched= " + movies)*/
+  const fetchMoviesHandler = async()=>{
+    try {
+     
+    const res = await axios.get('http://172.20.10.3:8081/users/api/v1')
+    console.log("here we are = " + res.data);
+
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  
+  useEffect(()=>{
+    fetchMoviesHandler()
+  }, [])
+
+  return <ProgramList programs={DUMMY_PROGRAMS} />;
 }
 
 export async function getStaticProps() {
   //fetch data from an API which code
   //we write here never end up in client side
   // the code here will never reach machines of our visitors
-
+  
   return {
     props: {
       programs: DUMMY_PROGRAMS,
