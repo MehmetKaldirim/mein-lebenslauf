33163089e01518c23c7d8ad606d33931bdc724c1
Mehmet <mathdenizi@gmail.com>
2023-01-07 19:46:14 +0100

    starting of using custom hook

--- pages/programs/[programId]/index.js
+++ pages/programs/[programId]/index.js
@@ -3,6 +3,9 @@ import { useRouter } from "next/router";
 function ProgramDetail() {
   const router = useRouter();
   const programId = router.query.programId;
+  //send a request to the backend API
+  //to fetch the news item with NewsId
+
   return <h1>Details about program {programId} </h1>;
 }
 
