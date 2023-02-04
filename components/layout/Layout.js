752712296ec311a58ab52a001f835d55a683f676
Mehmet <mathdenizi@gmail.com>
2023-02-05 00:36:33 +0100

    start add registration

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,15 +1,17 @@
 import { useSelector } from "react-redux";
 import MainNavigation from "./MainNavigation";
 import classes from "./Layout.module.css";
+import Register from "./Register";
 import Auth from "./Auth";
 
 function Layout(props) {
   const isAuth = useSelector((state) => state.auth.isAuthenticated);
 
+  //{!isAuth && <Auth />}
   return (
     <div>
       <MainNavigation />
-      {!isAuth && <Auth />}
+      {!isAuth && <Register />}
       <main className={classes.main}>{props.children}</main>
     </div>
   );
